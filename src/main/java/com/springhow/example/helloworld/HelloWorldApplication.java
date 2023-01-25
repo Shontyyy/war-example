7ae4fe5464125d83bbaa807baa40e65ddcaf7d89
Shontyyy <122144932+Shontyyy@users.noreply.github.com>
2023-01-26 03:33:38 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "salman khan";
+        return "Tarru haji saab";
     }
 
 }
