a1289aa50ce2ffc8c01d6225e50ea405fa48cb86
Shontyyy <122144932+Shontyyy@users.noreply.github.com>
2023-01-28 15:23:24 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t newimg .
-docker rm -f $(docker ps -a -q)
+docker rm -rf $(docker ps -a -q)
 docker container run -it --name webserver -p 8081:8080 newimg /bin/bash
 cd /$CATALINA_HOME/bin
 ./startup.sh
